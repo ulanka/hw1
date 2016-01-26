1e6edba500c8fc3576cd0001a4964408b83e96aa
Ulan Korabay <korabay@lvl-mmpl-6.ics.usc.edu>
2016-01-25 17:16:58 -0800

    Initial Commit

---
+++ hw1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw1
+//
+//  Created by Ulan Korabay on 1/25/16.
+//  Copyright (c) 2016 Ulan Korabay. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
