93da1dc434c07ba00d5ed790d5ff985df5eaae45
Ulan Korabay <korabay@lvl-mmpl-6.ics.usc.edu>
2016-01-25 18:05:52 -0800

    ttt

---
+++ hw1/printarguments.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+   std::cout << "argc = " << argc << std::endl;
+   for(int i = 0; i < argc; i++)
+      std::cout << "argv[" << i << "] = " << argv[i] << std::endl;
+   return 0;
+} 
